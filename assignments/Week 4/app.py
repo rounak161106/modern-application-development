13afc3f547277f731a4912277764f7cc295707ee
Rounak Prasad <rounak16112006@gmail.com>
2026-03-08 10:58:55 +0530

    Modified

--- assignments/Week 4/app.py
+++ assignments/Week 4/app.py
@@ -1,1 +1,14 @@
-  
+from flask import Flask
+from flask import render_template
+from flask import request
+import matplotlib
+
+app = Flask(__name__)
+# @app.route("/", method=["GET", "POST"])
+# def main()
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
