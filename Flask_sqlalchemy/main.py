4145cb42642b4ff0a0b8c78ce81a55696de7c8f9
Rounak Prasad <rounak16112006@gmail.com>
2026-03-20 15:52:04 +0530

    Flask sql alchemy demo

--- Flask_sqlalchemy/main.py
+++ Flask_sqlalchemy/main.py
@@ -8,6 +8,30 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(path, "testdb.sqlite3")
 db = SQLAlchemy()
 db.init_app(app)
-app.context().push()
+app.app_context().push()
 
+class Author(db.Model):
+    __tablename__ = "authors"
+    author_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    email = db.Column(db.String, unique=True)
 
+class Article(db.Model):
+    __tablename__ = "articles"
+    article_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    title = db.Column(db.String)
+    content = db.Column(db.String)
+    authors = db.relationship("Author", secondary="article_authors")
+
+class ArticleAuthor(db.Model):
+    __tablename__ = "article_authors"
+    author_id = db.Column(db.Integer, db.ForeignKey("authors.author_id"), primary_key=True, nullable=False)
+    article_id = db.Column(db.Integer, db.ForeignKey("articles.article_id"), primary_key=True, nullable=False)
+
+@app.route('/')
+def main():
+    articles = Article.query.all() #or db.session.query(Article).all()
+    return render_template("index.html", articles= articles)
+
+if __name__ == "__main__":
+    app.run(debug=True)
