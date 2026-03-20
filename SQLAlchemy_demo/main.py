197c00f9fe72478e49f489dae36dbfb4d500b1f5
Rounak Prasad <rounak16112006@gmail.com>
2026-03-20 12:24:01 +0530

    Add classes

--- SQLAlchemy_demo/main.py
+++ SQLAlchemy_demo/main.py
@@ -9,13 +9,21 @@ from sqlalchemy.orm import Session
 Base = declarative_base()
 
 class Author(Base):
-    __tablename__ = "Authors"
-    id = Column(Integer, autoincrement=True, primary_key=True)
+    __tablename__ = "authors"
+    author_id = Column(Integer, autoincrement=True, primary_key=True)
     name = Column(String, unique=True)
     email = Column(String, unique=True)
 
 class Article(Base):
-    __tablename__ = "Articles"
-    id = Column(Integer, autoincrement=True, primary_key=True)
-    name = Column(String, unique=True)
-    email = Column(String, unique=True)
+    __tablename__ = "articles"
+    article_id = Column(Integer, autoincrement=True, primary_key=True)
+    title = Column(String)
+    content = Column(String)
+    authors = relationship("Author", secondary="article_authors")
+
+class ArticleAuthor(Base):
+    __tablename__ = "article_authors"
+    author_id = Column(Integer, ForeignKey("authors.author_id"), primary_key=True, nullable=False)
+    article_id = Column(Integer, ForeignKey("articles.article_id"), primary_key=True, nullable=False)
+
+engine = create_engine("sqlite:///testdb.sqlite3")
