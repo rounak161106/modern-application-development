3ae3b592fd4962b5027c47687cea249fe31550d6
Rounak Prasad <rounak16112006@gmail.com>
2026-03-20 14:24:12 +0530

    Transaction control

--- SQLAlchemy_demo/main.py
+++ SQLAlchemy_demo/main.py
@@ -28,17 +28,39 @@ class ArticleAuthor(Base):
 
 engine = create_engine("sqlite:///./SQLAlchemy_demo/testdb.sqlite3")
 
-if __name__ == "__main__":
-    stmt = select(Author)
-    print(stmt)
-    with engine.connect() as conn:
-        for row in conn.execute(stmt):
-            print(row)
 
-    # find the author name who are related with article id 1
-    with Session(engine) as session:
-        articles = session.query(Article).filter(Article.article_id == 1).all()
-        for article in articles:
-            print(article.title)
-            for author in article.authors:
-                print(author.name)
+# Basic querying
+# if __name__ == "__main__":
+#     stmt = select(Author)
+#     print(stmt)
+#     with engine.connect() as conn:
+#         for row in conn.execute(stmt):
+#             print(row)
+
+#     # find the author name who are related with article id 1
+#     with Session(engine) as session:
+#         articles = session.query(Article).filter(Article.article_id == 1).all()
+#         for article in articles:
+#             print(article.title)
+#             for author in article.authors:
+#                 print(author.name)
+
+
+# Transaction Handling
+if __name__ == "__main__":
+    with Session(engine, autoflush=False) as session:
+        session.begin()
+        try:
+            article = Article(title = "Dummy article", content = "This is dummy content")
+            session.add(article)
+            session.flush()
+            # raise Exception("Dummy error")
+            article_author = ArticleAuthor(author_id = 1, article_id = article.article_id)
+            session.add(article_author)
+        except:
+            print("Rolling Back")
+            session.rollback()
+            raise
+        else:
+            print("Commit")
+            session.commit()
