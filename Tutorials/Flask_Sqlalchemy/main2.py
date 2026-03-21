1c2ee6d4422384f32dcafe1603896da2507cc14f
Rounak Prasad <rounak16112006@gmail.com>
2026-03-21 23:08:06 +0530

    Finalized the tutorials of flask sqlalchemy

--- Tutorials/Flask_Sqlalchemy/main2.py
+++ Tutorials/Flask_Sqlalchemy/main2.py
@@ -8,19 +8,24 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///manymany.sqlite3"
 db = SQLAlchemy(app)
 app.app_context().push()
 
-# ========== models for one to many relationship ===============
+# ========== models for many to many relationship ===============
 
-# Child Table (many side)
 class User(db.Model):
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key = True, autoincrement = True)
     name = db.Column(db.String, nullable = False, unique = True)
     password = db.Column(db.String, nullable = False)
+    roles = db.relationship("Role", backref="users", secondary="association")
 
-# Parent Table (one side)
 class Role(db.Model):
     __tablename__ = "role"
     id = db.Column(db.Integer, primary_key = True, autoincrement = True)
     role_name = db.Column(db.String, nullable = False, unique = True)
-    users = db.relationship("User", backref = "role")         #This backref = role will give the functionality to the User model to access the role attributes 
- 
+    
+
+class Association(db.Model):
+    __tablename__ = "association"
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    role_id = db.Column(db.Integer, db.ForeignKey("role.id"))
+
