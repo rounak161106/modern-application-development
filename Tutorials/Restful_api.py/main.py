cd26309bda75f488d345ece15c8d71f4ac047313
Rounak Prasad <rounak16112006@gmail.com>
2026-03-26 13:19:04 +0530

    Add the post method and the get method

--- Tutorials/Restful_api.py/main.py
+++ Tutorials/Restful_api.py/main.py
@@ -1,11 +1,17 @@
-from flask_restful import Resource, Api, fields, marshal_with
-from flask import Flask, make_response
+from flask_restful import Resource, Api, fields, marshal_with, reqparse
+from flask import Flask, make_response, request
 from flask_sqlalchemy import SQLAlchemy
+import json
 from werkzeug.exceptions import HTTPException    #to handle exceptions
 
 class NotFoundError(HTTPException):
     def __init__(self, status_code):
         self.response = make_response('', status_code)
+
+class BusinessValidationError(HTTPException):
+    def __init__(self, status_code, error_code, error_message):
+        message = {"error_code" : error_code, "error_message" : error_message}
+        self.response = make_response(json.dumps(message), status_code)
 
 output_fields = {
     "author_id" : fields.Integer, 
@@ -22,6 +28,10 @@ db = SQLAlchemy(app)
 app.app_context().push()
 
 api = Api(app)
+
+create_user_parser = reqparse.RequestParser()
+create_user_parser.add_argument('username')
+create_user_parser.add_argument('email')
 
 class Author(db.Model):
     __tablename__ = "authors"
@@ -58,8 +68,18 @@ class UserApi(Resource):
     def delete(self, username):
         pass
 
-    def push(self):
-        pass
+    def post(self):
+        args = create_user_parser.parse_args()
+        name = args.get("username", None) 
+        email = args.get("email", None) 
+
+        if not name or not email:
+            raise BusinessValidationError(status_code=404, error_code="BE1001", error_message="Shouldn't be none")
+
+        new_user_obj = Author(name = name, email = email)
+        db.session.add(new_user_obj)
+        db.session.commit()
+        return {"Message": "Added"},201
 
 api.add_resource(UserApi, "/api/user" , "/api/user/<string:username>")
 
