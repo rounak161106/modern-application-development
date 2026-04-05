75187c88845b5a7a807c05a403057914a648e405
Rounak Prasad <rounak16112006@gmail.com>
2026-04-05 21:02:06 +0530

    Connect to postgres database

--- Psycopg/intro.py
+++ Psycopg/intro.py
@@ -5,3 +5,28 @@
 # close the cursor 
 # close the connection
 
+import psycopg2
+
+try:
+    conn = psycopg2.connect(
+        host="localhost",
+        database="flis",
+        user="postgres",
+        password="Rounak@123",
+        port="5432"   # default PostgreSQL port
+    )
+
+    print("Connected successfully!")
+
+    cur = conn.cursor()
+
+    # Example query
+    cur.execute("SELECT * from players;")
+    result = cur.fetchall()
+    print(result)
+
+    cur.close()
+    conn.close()
+
+except Exception as e:
+    print("Error: 😭", e)
