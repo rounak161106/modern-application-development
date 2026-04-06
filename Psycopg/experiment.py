a2d1616137f7e988850fa49a8f92f25a8913956d
Rounak Prasad <rounak16112006@gmail.com>
2026-04-06 09:10:19 +0530

    Create new table using psycopg2

---
+++ Psycopg/experiment.py
@@ -0,0 +1,21 @@
+import psycopg2
+
+conn = psycopg2.connect(host = "localhost", database = "demo", user = "postgres", password = "Rounak@123", port = "5432")
+print("Connection Successful!")
+
+cur = conn.cursor()
+cur.execute("""Create table table3( id int primary key, name varchar(20), city varchar(20))""")
+print("Table created successfully!")
+
+cur.execute("""Insert into table3 values(1, 'Rounak', 'Bangalore')""")
+cur.execute("""Insert into table3 values(2, 'Rahul', 'Delhi')""")
+cur.execute("""Insert into table3 values(3, 'Rohit', 'Mumbai')""")
+print("Data inserted successfully!")
+
+cur.execute("Select * from table3;")
+result = cur.fetchall()
+
+conn.commit()
+cur.close()
+conn.close()
+print(result)
