84bf3ea2027c4749bbb547713514b2c9b8fbdd2c
Rounak Prasad <rounak16112006@gmail.com>
2026-04-15 23:05:44 +0530

    Fix jersey_no.py

--- Psycopg/jersey_no.py
+++ Psycopg/jersey_no.py
@@ -2,5 +2,5 @@ import psycopg2
 
 conn = psycopg2.connect(database = "flis", user = "postgres", password = "Rounak@123", port = 5432, host = "localhost")
 cur = conn.cursor()
-cur.execute("Select * from players where name = '{}'".format("Shlok"))
-print(cur.fetchall())
+cur.execute("Select jersey_no from players where name = '{}'".format("Shlok"))
+print(cur.fetchone()[0])
