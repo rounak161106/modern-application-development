efafe5287905c572af59c78b0700b516424f75b3
Rounak Prasad <rounak16112006@gmail.com>
2026-04-16 22:19:43 +0530

    Create session

--- Flask_Login/app.py
+++ Flask_Login/app.py
@@ -1,20 +1,24 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, session
 
 app = Flask(__name__)
-
+app.config["SECRET_KEY"] = "thisisasecretkey"
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form.get("username")
         password = request.form.get("password")
+        # loading user creadentials into the session 
+        session["username"] = username
+        session["password"] = password
         return redirect("/dashboard")
     
     return render_template("login_form.html")
 
 @app.route('/dashboard')
 def dashboard():
-    return "Login Successful!"
+    username = session.get("username")
+    return f"Welcome {username}!"
 
 if __name__ == "__main__":
     app.run(debug=True)
