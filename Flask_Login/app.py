065140870eeae4eb27db6c451635b9737ef86c47
Rounak Prasad <rounak16112006@gmail.com>
2026-04-16 22:33:54 +0530

    End tutorial

--- Flask_Login/app.py
+++ Flask_Login/app.py
@@ -8,7 +8,7 @@ def login():
     if request.method == 'POST':
         username = request.form.get("username")
         password = request.form.get("password")
-        # loading user creadentials into the session 
+        # loading user credentials into the session 
         session["username"] = username
         session["password"] = password
         return redirect("/dashboard")
@@ -20,5 +20,13 @@ def dashboard():
     username = session.get("username")
     return f"Welcome {username}!"
 
+@app.route('/logout')
+def logout():
+    session.pop("username", None)
+    session.pop("password", None)
+    return redirect('/login')
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+# eyJwYXNzd29yZCI6IjEyMzQ1IiwidXNlcm5hbWUiOiJSb3VuYWsifQ.aeEVRA.1U-zne3Xvcv6kY7SEFgitFxJsvI eyJwYXNzd29yZCI6IjEyMzQ1IiwidXNlcm5hbWUiOiJSb3VuYWsifQ.aeEVeA.29ra7hfGJPYRYDB_AY6PpuCT6XE
