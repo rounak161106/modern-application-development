ec1104543b56162119ef1813bd1fd2bf59c6c763
Rounak Prasad <rounak16112006@gmail.com>
2026-04-18 23:00:54 +0530

    Add Guide.txt

--- Testing/test_intro.py
+++ Testing/test_intro.py
@@ -1,18 +1,3 @@
-# Standard naming convention(mandatory)
-# Filename : start or end with test_ or _test
-# Function name should be started with test keyword
-# Class name should start with Testxxxx
-
-#pytest running
-# pytest test_intro.py
-# pytest .
-# pytest test_intro.py -v (Verbose)
-# pytest -k "increment"   (k for keyword)
-# pytest test_intro.py::test_increment     (Note that test_idtro is file name, increment and test_increment is function name)
-
-
-
-
 def increment(x):
     return x+1;
 
