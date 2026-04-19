0c7283901f489294cb7e14ab73b6dc2dc169d5ca
Rounak Prasad <rounak16112006@gmail.com>
2026-04-19 22:19:15 +0530

    Display DataFrame

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -2,4 +2,15 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+
 st.title("Hello Streamlit")
+
+st.write("Here is the DataFrame")
+
+df = pd.DataFrame({
+    "Name" : ["Rounak", "Tanya", "Jay"], 
+    "Age" : [18,19,15]
+})
+
+st.write(df)
+
