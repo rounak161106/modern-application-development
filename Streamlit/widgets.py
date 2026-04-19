588aeb00a9972220695b34ab83c2aa2ef85fbbd1
Rounak Prasad <rounak16112006@gmail.com>
2026-04-19 22:33:04 +0530

    Add age slider

--- Streamlit/widgets.py
+++ Streamlit/widgets.py
@@ -2,3 +2,7 @@ import streamlit as st
 st.title("Streamlit text input")
 name =  st.text_input("Enter your name : ")
 st.write(f'Hello {name}')
+
+#add slider to take age as input
+age = st.slider("Select your age : ",10,80,15)
+st.write(f'Your age is {age}')
