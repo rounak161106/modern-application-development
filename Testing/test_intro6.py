39b812f3644968324f7f29dc6f90962a5e8445f9
Rounak Prasad <rounak16112006@gmail.com>
2026-04-19 22:10:56 +0530

    Add streamlit folder

--- Testing/test_intro6.py
+++ Testing/test_intro6.py
@@ -5,4 +5,10 @@ def test_get():
     print("%$@#$%@#$%@#")
     print(res)
     assert res.status_code == 200
-test_get()
+
+def test_post():
+    end_url="http://127.0.0.1:5000/add"
+    pay_load = {"id" :153, "name":"rdfa"}
+    headers = {"content-type":"application/json"}
+    res=requests.post(end_url, json=pay_load,headers=headers)
+    assert res.status_code == 201
